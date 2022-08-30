b59c3afc18e2c00ba09977b2472794cd5e359d60
wewe15 <ahm.sami.271@gmail.com>
2022-08-30 14:51:47 +0200

    Added rank endpoint

--- server/handelrs/rank.js
+++ server/handelrs/rank.js
@@ -1,10 +1,30 @@
 import { data } from '../testData.js';
 
-let scores = data.scoresList
+let scores = data.scoresList;
 
-const createRank = async (_req, res) => {
+const getRank = (arr, num) => {
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++){
+        let score = arr[i];
+
+        if (score < num) count++;
+    }
+
+    let rank = Number(((count / arr.length) * 100).toFixed(2))
+
+    return rank
+}
+
+const createRank = async (req, res) => {
     try{
+        const score  = req.body.score
 
+        if(!score) return res.status(404).json("score should not be empty");
+
+        let rank = getRank(scores, score)
+
+        res.status(201).json(rank)
     } catch (err){
         res.status(400).json(err);
     }
